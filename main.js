f082a34dbc1320a952a508bac30ded963933eaa7
Marshy <100256954+bruh0meme@users.noreply.github.com>
2022-04-08 17:41:15 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","Me.jpg", "Sis.jpeg" , "daddy.jpeg", "mom.jpeg" ];
+var names = ["Family Book","Pranesh", " Vedhikaa", "Thiru", "Sujatha"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 4
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    
+    var updatedImage = images[i];
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
